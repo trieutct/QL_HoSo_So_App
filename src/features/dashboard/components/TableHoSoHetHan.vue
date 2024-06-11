cc894b72948c7ff4e1c5262ccdf6a1637c374fb0
trieutct <96654274+trieutct@users.noreply.github.com>
2024-06-11 22:30:35 +0700

    done

--- src/features/dashboard/components/TableHoSoHetHan.vue
+++ src/features/dashboard/components/TableHoSoHetHan.vue
@@ -1,20 +1,12 @@
 <template>
   <el-card class="box-card">
     <p class="text-base font-bold">Danh sách hồ sơ hết hạn</p>
-    <el-table
-      class="mt-4"
-      :height="'calc(220px)'"
-      :data="expricedHoSo"
-      border
-      width="100%"
-    >
+    <el-table class="mt-4" :height="'calc(220px)'" :data="expricedHoSo" border width="100%">
       <el-table-column fixed type="selection" width="55" />
       <el-table-column prop="fileCode" label="Mã hồ sơ" width="100" />
       <el-table-column prop="title" label="Tiêu đề" width="300">
         <template #default="scope">
-          <router-link
-            :to="{ name: PageName.DOC_PAGE, params: { id: scope.row.id } }"
-          >
+          <router-link :to="{ name: PageName.DOC_PAGE, params: { id: scope.row.id } }">
             <p>
               {{ scope.row.title }}
             </p>
@@ -23,11 +15,7 @@
       </el-table-column>
       <el-table-column prop="keyword" label="Từ khóa" width="200" />
       <el-table-column prop="fileCatalog" label="Mục lục số" width="150" />
-      <el-table-column
-        prop="fileNotation"
-        label="Số và ký hiệu hồ sơ"
-        width="180"
-      />
+      <el-table-column prop="fileNotation" label="Số và ký hiệu hồ sơ" width="180" />
       <el-table-column prop="maintenance" label="Số lượng tờ" width="150" />
       <el-table-column prop="pageNumber" label="Số lượng trang" width="150" />
       <el-table-column prop="totalDoc" label="Tổng số văn bản" width="150" />
@@ -43,7 +31,7 @@
         </template>
       </el-table-column>
       <el-table-column prop="description" label="Mô tả" width="400" />
-      <el-table-column
+      <!-- <el-table-column
         fixed="right"
         label="Hành Động"
         align="center"
@@ -52,7 +40,7 @@
         <template #default="scope">
           <el-button type="danger" :icon="Delete" circle />
         </template>
-      </el-table-column>
+      </el-table-column> -->
     </el-table>
   </el-card>
 </template>
@@ -67,6 +55,6 @@ const expricedHoSo = computed(() => dashboardDocStore.expricedHoSo);
 </script>
 <style scoped>
 .text-base {
-  color: #737992;
+  color: black;
 }
 </style>
